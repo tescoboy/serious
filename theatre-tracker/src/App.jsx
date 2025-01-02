25aa11a7f49452f5612395b4958a19cdf7015547
tescoboy <43451876+tescoboy@users.noreply.github.com>
2025-01-02 15:01:08 +0000

    Create App.jsx

---
+++ theatre-tracker/src/App.jsx
@@ -0,0 +1,11 @@
+import TheatreTracker from './components/TheatreTracker';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-slate-900 to-red-950">
+      <TheatreTracker />
+    </div>
+  );
+}
+
+export default App;
